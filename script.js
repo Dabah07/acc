f5b40341b32a7250940709ce2d8e93792b97c59e
dabah aissa <qyosk727@gmail.com>
2024-10-07 13:29:07 +0200

    none

---
+++ script.js
@@ -0,0 +1,8 @@
+let prodoui_cart = document.getElementsByClassName("product-card")
+let totalquantity = document.getElementById("total-quantity")
+let totalprice =document.getElementById("total-price")
+
+
+for (prodoui_cart of prodoui_carts) {
+    let  totalquantity = prodoui_cart.quersilactor("")
+}
